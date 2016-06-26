07eaf4f67f46323164647539ba28b5af12ce523f
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2016-06-26 12:27:54 -0700

    added a couple more modules

---
+++ how_you_could_use_nimble.R
@@ -0,0 +1,7 @@
+## ----chunksetup, include=FALSE-------------------------------------------
+# Following code is only needed for slide generation, not for using R code separately.
+library(methods)
+
+## ----loadnimble, include=FALSE-------------------------------------------
+library(nimble)
+
